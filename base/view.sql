211e280a46bee616a1aa40867c5b517e9d936aad
thonyETU1883 <thonyrasolofondraibe1883@gmail.com>
2023-11-15 08:49:24 +0100

    view

--- base/view.sql
+++ base/view.sql
@@ -1,7 +1,7 @@
 CREATE VIEW etatdestock as
 SELECT idmouvement,date,m.idarticle,
 (SELECT SUM(m1.quantiteentree)-SUM(m1.quantitesortie) FROM mouvement m1 WHERE m1.date<=m.date AND m1.idarticle = m.idarticle AND m1.idmagasin = m.idmagasin) as quantitereste,
-(SELECT SUM(m1.prixunitaire*m1.quantiteentree)/SUM(m1.quantiteentree) FROM mouvement m1 WHERE m1.date<=m.date AND m1.idarticle = m.idarticle AND m1.idmagasin = m.idmagasin) as moyenneprixunitaire,
+(SELECT SUM(m1.prixunitaire*m1.quantiteentree)/SUM(m1.quantiteentree) FROM mouvement m1 WHERE m1.quantitesortie = 0 AND m1.date<=m.date AND m1.idarticle = m.idarticle AND m1.idmagasin = m.idmagasin) as moyenneprixunitaire,
 m.idmagasin,article.nomarticle,magasin.nommagasin
 FROM 
 mouvement as m
